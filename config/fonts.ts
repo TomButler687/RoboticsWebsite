c7ddd815c88bc2bf9323f0e389d866e35471572e
Tom Butler <thomasbutler687@gmail.com>
2025-06-10 19:54:10 +0100

    Fonts, finally?

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,13 @@
-// import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import localFont from 'next/font/local';
 
-// export const fontSans = FontSans({
-//   subsets: ["latin"],
-//   variable: "--font-sans",
-// });
+export const fontSans = localFont({
+  src: '../public/fonts/Inter/Inter-VariableFont_opsz,wght.ttf',
+  variable: '--font-sans',
+  display: 'swap',
+});
 
-// export const fontMono = FontMono({
-//   subsets: ["latin"],
-//   variable: "--font-mono",
-// });
+export const fontMono = localFont({
+  src: '../public/fonts/FiraCode/FiraCode-VariableFont_wght.ttf',
+  variable: '--font-mono',
+  display: 'swap',
+});
